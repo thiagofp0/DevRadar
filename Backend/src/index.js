6d6f0bcd99e82230c43fad50f57098f0801b7dba
Thiago <thiagofp707@gmail.com>
2020-01-14 17:50:45 -0300

    Routes file configuring

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://thiago:thiago@cluster0-1ffee.mongodb.net/week10?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(express.json());
+app.use(routes);
 
 // get, post, put, delete
 
@@ -14,9 +16,6 @@ app.use(express.json());
 //Route Params - Usa no put e no delete - request.params - (Identificar um recurso na alteração ou remoção)
 //Body - Usa no Post e Put - request.body - (Dados para criação ou alteração de um registro)
 
-app.post('/users/:id', (request, response) =>{
-    console.log(request.body);
-     return response.json({message: 'Hello OmniStack'});
-});
+
 
 app.listen(3333);
